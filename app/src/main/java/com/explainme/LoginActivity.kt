305c7c62dec58c1f8db50d3d7bc3e8ce32081802
Damir Petrov <petrovdamir2235@gmail.com>
2020-08-08 21:05:30 +0500

    Add registration

--- app/src/main/java/com/explainme/LoginActivity.kt
+++ app/src/main/java/com/explainme/LoginActivity.kt
@@ -1,16 +1,19 @@
 package com.explainme
-
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
-import com.google.android.material.snackbar.Snackbar
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 
 class LoginActivity : AppCompatActivity() {
@@ -22,12 +25,11 @@ class LoginActivity : AppCompatActivity() {
         val gso =
             GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
-                .requestProfile()
                 .build()
-        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso) ;
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
         findViewById<View>(R.id.sign_in_button).setOnClickListener {
-            val signInIntent: Intent = mGoogleSignInClient.getSignInIntent()
-            startActivityForResult(signInIntent, 5)
+            val signInIntent: Intent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, 0)
         }
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -35,9 +37,39 @@ class LoginActivity : AppCompatActivity() {
         Log.i("ExplainMe", "onActivityResult")
         val task = GoogleSignIn.getSignedInAccountFromIntent(data)
         val account = task.getResult(ApiException::class.java)!!
-        Snackbar.make(findViewById<View>(R.id.sign_in_button), "Hi " + account.displayName, Snackbar.LENGTH_SHORT)
-            .show()
-        Log.i("ExplainMe", "onActivityResult" + account.id)
-
+        val id = account.id
+        val photoUrl = account.photoUrl!!
+        val email = account.email
+        val displayName = account.displayName
+        val jsonParam = JSONObject()
+        jsonParam.put("type", "add_user")
+        jsonParam.put("google_id", id)
+        jsonParam.put("google_mail", email)
+        jsonParam.put("display_name", displayName)
+        jsonParam.put("photo_url", photoUrl.toString())
+        Log.i("ExplainMe", jsonParam.toString())
+        thread { registerUser(jsonParam) }
+    }
+    private fun registerUser(user: JSONObject) {
+        val url = URL("http://192.168.1.6:8000")
+        val con = url.openConnection() as HttpURLConnection
+        con.requestMethod = "POST"
+        con.setRequestProperty("Content-Type", "application/json; utf-8")
+        con.setRequestProperty("Accept", "application/json")
+        con.doOutput = true
+        con.outputStream.use { os ->
+            val input: ByteArray = user.toString().toByteArray()
+            os.write(input, 0, input.size)
+        }
+        BufferedReader(
+            InputStreamReader(con.inputStream, "utf-8")
+        ).use { br ->
+            val response = StringBuilder()
+            var responseLine: String?
+            while (br.readLine().also { responseLine = it } != null) {
+                response.append(responseLine!!.trim { it <= ' ' })
+            }
+            println(response.toString())
+        }
     }
 }
