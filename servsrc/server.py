fd6454b599edc69e731bd602bf7c807666008b97
dword <camm2016@yandex.ru>
2020-08-09 05:23:18 +0300

    + zoom_create_meeting()

--- servsrc/server.py
+++ servsrc/server.py
@@ -4,21 +4,43 @@ import logging
 import json
 import sqlite3
 from database import Dbase
-import cgi
+import requests
 
 dbase = Dbase()
 
 class Serv(http.server.BaseHTTPRequestHandler):
     def handle_add_user(self, message):
         dbase.add_user(message)
+        self._set_headers()
 
 
     def handle_add_lecture(self, message):
         dbase.add_lecture(message)
+        self._set_headers()
 
 
     def handle_get_lectures(self, message):
-        return dbase.get_lectures(message['begin'], message['end'])
+        lectures = dbase.get_lectures(message['begin'], message['end'])
+        self._set_headers()
+        response = json.dumps(lectures).encode('utf-8')
+        logging.info(response)
+        self.wfile.write(response)
+
+
+    def zoom_create_meeting_request(self, lecture):
+        jwt_token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6Im5WZlZ1WUNfVE51X09OQTRzTC13ZlEiLCJleHAiOjE1OTc1NDMwODQsImlhdCI6MTU5NjkzODI4NH0._x0QFJsokMDorcrlrh0B1SssP7dn4uJrpR3NwV6b6yY'
+        heads = {'Authorization': jwt_token, 'Content-type': 'application/json'}
+        body = {
+            'topic': lecture['title'],
+            'type': 2,
+            'timezone': 'UTC',
+            'agenda': lecture['description']
+        }
+        user_id = '3akLmEOxSaaFh6v_F9t4zQ'
+        url = 'https://api.zoom.us/v2/users/' + user_id + '/meetings'
+        response = requests.post(url, headers = heads, json = body)
+        json_response = response.json()
+        logging.info(json_response)
 
 
     def _set_headers(self):
@@ -28,6 +50,8 @@ class Serv(http.server.BaseHTTPRequestHandler):
 
 
     def do_HEAD(self):
+        lect = {'title': 'aaaa', 'description': 'bbbb', 'author': 'cccc', 'time': 213134}
+        self.zoom_create_meeting_request(lect)
         self._set_headers()
 
 
@@ -47,18 +71,10 @@ class Serv(http.server.BaseHTTPRequestHandler):
 
         if message.get('type') == 'add_user':
             self.handle_add_user(message)
-            self._set_headers()
         elif message.get('type') == 'add_lecture':
             self.handle_add_lecture(message)
-            self._set_headers()
         elif message.get('type') == 'get_lectures':
-            lectures = self.handle_get_lectures(message)
-            self._set_headers()
-            response = json.dumps(lectures).encode('utf-8')
-            logging.info(response)
-            self.wfile.write(response)
-
-
+            self.handle_get_lectures(message)
 
 
 
